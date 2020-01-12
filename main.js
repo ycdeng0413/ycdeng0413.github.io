91a2b008776d21d23679513d789f505d2e0ac1db
Yu-Cheng Deng <49569296+ycdeng0413@users.noreply.github.com>
2020-01-11 23:14:09 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-var myHeading = document.querySelector('h1');
+var myHeading = document.querySelector('h1').style.backgroundColor = "red";
 myHeading.textContent = "I'm currently a Master's student majoring in Electrical and Computer Engineering Department at Univeristy of California Davis";
