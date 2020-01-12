2d35d43c15f9f34247b8e51c55a9abc02fe4d9cf
Yu-Cheng Deng <49569296+ycdeng0413@users.noreply.github.com>
2020-01-11 23:13:10 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = "I'm currently a Master's student majoring in Electrical and Computer Engineering Department at Univeristy of California Davis";
